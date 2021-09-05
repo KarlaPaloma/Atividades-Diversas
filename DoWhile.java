aa63b7d572d0fb3a2eb26e629bf8930c2440074c
KarlaPaloma <karlapaloma1989@hotmail.com>
2021-09-05 12:37:11 -0300

    Contando com Do While

---
+++ DoWhile.java
@@ -0,0 +1,18 @@
+package Logica;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+	int cont = 1;
+	do {
+		System.out.println(cont);
+		cont = cont + 1;
+	}while (cont <= 5);
+	
+	System.out.println("FIM!");
+	
+	}
+//Contador está imprimindo os número de 1 até o 5.
+
+	}
