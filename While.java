b1c01707ddcc211678a6d77782a61b52d5fc8d4b
KarlaPaloma <karlapaloma1989@hotmail.com>
2021-09-05 12:34:18 -0300

    Contando com While

---
+++ While.java
@@ -0,0 +1,17 @@
+package Logica;
+
+public class While {
+	
+	//Enquanto o i for menor que 0, imprima o i, depois some o i mais 1.
+	//lembrando que o i começa com o número 1.
+	
+	public static void main(String[] args) {
+	
+		int i = 1;
+		while (i < 10) {
+		    System.out.println(i);
+		    i++;
+		}
+		
+	}
+}
